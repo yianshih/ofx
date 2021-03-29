ed369e2f761493f7983168b085faec61ded869bb
Lumizz <yiansh15@gmail.com>
2021-03-29 23:04:53 +1100

    fix svg module bugs

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,5 @@
 import React from "react"
 import ReactDOM from "react-dom"
-
-import reportWebVitals from "./reportWebVitals"
 import WanringProvider from "./HOC/WarningProvider/WarningProvider"
 import LoadingProvider from "./HOC/LoadingProvider/LoadingProvider"
 import { Provider } from "react-redux"
@@ -21,8 +19,3 @@ ReactDOM.render(
   </Provider>,
   document.getElementById("root")
 )
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
